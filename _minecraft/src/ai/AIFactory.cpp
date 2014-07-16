a3ef704ea609dde05b2300561693fd63632eb1e4
Daniel Quach <mevoroth@gmail.com>
2014-07-16 23:54:18 +0200

    parasite behavior

--- _minecraft/src/ai/AIFactory.cpp
+++ _minecraft/src/ai/AIFactory.cpp
@@ -18,10 +18,31 @@ const BehaviorTree* AIFactory::createParasiteAI()
 {
 	if (!_parasiteAI)
 	{
+		Behavior* behavior;
+		Behavior* behaviorSub;
+
 		_parasiteAI = new BehaviorTree();
 		
 		ParasiteSelector* parasiteSel = new ParasiteSelector();
-		
+
+		behavior = new ParasitingBehavior();
+		behavior->addSubBehavior(new ParasitingCondition());
+		behavior->addSubBehavior(new Parasiting());
+		behavior->addSubBehavior(new Multiply());
+		parasiteSel->addBehavior(behavior);
+
+		behavior = new HuntBehavior();
+
+		behaviorSub = new Find();
+		behavior->addSubBehavior(behaviorSub);
+
+		behaviorSub = new MoveRandomly();
+		behaviorSub->addSubBehavior(new FindRandomDirection());
+		behaviorSub->addSubBehavior(new Teleport());
+		behavior->addSubBehavior(behaviorSub);
+
+		parasiteSel->addBehavior(behavior);
+
 		_parasiteAI->setSelector(parasiteSel);
 	}
 	return _parasiteAI;
